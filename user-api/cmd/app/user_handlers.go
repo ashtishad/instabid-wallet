931cf61ec1915f89c062bf2dd697d892ca2c940c
ashtishad <ashefgo@gmail.com>
2023-09-24 00:59:41 +0600

    Added handler for create user

--- user-api/cmd/app/user_handlers.go
+++ user-api/cmd/app/user_handlers.go
@@ -1,1 +1,38 @@
 package app
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/ashtishad/instabid-wallet/user-api/internal/domain"
+	"github.com/ashtishad/instabid-wallet/user-api/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandlers struct {
+	s service.UserService
+}
+
+func (uh *UserHandlers) NewUserHandler(c *gin.Context) {
+	var newUserRequest domain.NewUserReqDTO
+	if err := c.ShouldBindJSON(&newUserRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+		return
+	}
+
+	timeoutCtx, cancel := context.WithTimeout(c.Request.Context(), 500*time.Millisecond)
+	defer cancel()
+
+	res, apiErr := uh.s.NewUser(timeoutCtx, newUserRequest)
+	if apiErr != nil {
+		c.JSON(apiErr.Code(), gin.H{
+			"error": apiErr.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"user": &res,
+	})
+}
