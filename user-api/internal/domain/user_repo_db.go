5792cf9112f3c301896d1d9b42e51b69cc8c7bed
ashtishad <ashefgo@gmail.com>
2023-09-29 14:40:46 +0600

    Implemented InsertProfile and findProfile methods in repository db. * InsertProfile has transaction with level read committed.

--- user-api/internal/domain/user_repo_db.go
+++ user-api/internal/domain/user_repo_db.go
@@ -56,6 +56,77 @@ func (d *UserRepoDB) Insert(ctx context.Context, u User) (*User, lib.APIError) {
 	return d.findByUUID(ctx, u.UserID)
 }
 
+// InsertProfile takes uuid as string of user, and inserts profile to that specific user.
+// creates user profile in a transaction with isolation level read committed, returns *Profile
+// if error happens it returns 404,500
+func (d *UserRepoDB) InsertProfile(ctx context.Context, uuid string, up Profile) (*Profile, lib.APIError) {
+	id, apiErr := d.findIDByUUID(ctx, uuid)
+	if apiErr != nil {
+		return nil, apiErr
+	}
+
+	tx, err := d.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelReadCommitted})
+	if err != nil {
+		d.l.ErrorContext(ctx, lib.ErrTXBegin, "err", err.Error())
+		return nil, lib.InternalServerError(lib.ErrUnexpectedDatabase, err)
+	}
+
+	defer rollbackOnError(tx, &err, d.l)
+
+	sqlInsertProfile := `INSERT into user_profiles (user_id, first_name, last_name, gender, address) 
+						values ($1, $2, $3, $4, $5)`
+
+	var res sql.Result
+	res, err = tx.ExecContext(ctx, sqlInsertProfile, id, up.FirstName, up.LastName, up.Gender, up.Address)
+
+	if err != nil {
+		d.l.ErrorContext(ctx, lib.ErrScanRow, "err", err.Error())
+		return nil, lib.InternalServerError(lib.ErrUnexpectedDatabase, err)
+	}
+
+	var ra int64
+	ra, err = res.RowsAffected()
+
+	if err != nil {
+		d.l.ErrorContext(ctx, "unable to get rows affected", "err", err.Error())
+		return nil, lib.InternalServerError(lib.ErrUnexpectedDatabase, err)
+	}
+
+	if ra != 1 {
+		err = fmt.Errorf("expected to affect 1 row, affected %d", ra)
+		d.l.WarnContext(ctx, err.Error())
+
+		return nil, lib.InternalServerError(lib.ErrUnexpectedDatabase, err)
+	}
+
+	if err = tx.Commit(); err != nil {
+		d.l.ErrorContext(ctx, lib.ErrTXCommit, "err", err.Error())
+		return nil, lib.InternalServerError(lib.ErrUnexpectedDatabase, err)
+	}
+
+	return d.findProfile(ctx, id)
+}
+
+// findIDByUUID retrieves user id int64 from user uuid
+// returns 404 and 500 if error happens.
+func (d *UserRepoDB) findIDByUUID(ctx context.Context, userID string) (int64, lib.APIError) {
+	sqlFindIDByUUID := `SELECT id from users where user_id = $1`
+	var id int64
+
+	err := d.db.QueryRowContext(ctx, sqlFindIDByUUID, userID).Scan(&id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, lib.NotFoundError("user not found by uuid")
+		}
+
+		d.l.ErrorContext(ctx, "failed to find id by uuid", "err", err.Error())
+
+		return 0, lib.InternalServerError(lib.ErrUnexpectedDatabase, err)
+	}
+
+	return id, nil
+}
+
 // findByUUID retrieves a user by their UUID from the database.
 // If the user is not found, a NotFoundError is returned, Any other errors result in an InternalServerError.
 func (d *UserRepoDB) findByUUID(ctx context.Context, uuid string) (*User, lib.APIError) {
@@ -75,6 +146,27 @@ func (d *UserRepoDB) findByUUID(ctx context.Context, uuid string) (*User, lib.AP
 	}
 
 	return &u, nil
+}
+
+// findProfile retrieves a user profile by their user id from the database.
+// If the user profile is not found, a NotFoundError is returned, Any other errors result in an InternalServerError.
+func (d *UserRepoDB) findProfile(ctx context.Context, id int64) (*Profile, lib.APIError) {
+	sqlFindByUUID := `SELECT  first_name, last_name, gender, address, created_at, updated_at
+					 from user_profiles where user_id= $1`
+
+	var up Profile
+	row := d.db.QueryRowContext(ctx, sqlFindByUUID, id)
+
+	err := row.Scan(&up.FirstName, &up.LastName, &up.Gender, &up.Address, &up.CreatedAt, &up.UpdatedAt)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, lib.NotFoundError("user profile not found by user id")
+		}
+
+		return nil, lib.InternalServerError(lib.ErrUnexpectedDatabase, err)
+	}
+
+	return &up, nil
 }
 
 // checkExists checks user email or username exists in database, if any of these exists, it will return an error
